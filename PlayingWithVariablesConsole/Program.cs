d18dd2fb5542bcbfe488faa48021b4d29c86d421
brianna audain <briannaaudain2@outlook.com>
2022-03-25 11:46:23 -0500

    finished project

---
+++ PlayingWithVariablesConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PlayingWithVariablesConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int age;
+			string name;
+
+			bool isAlive;
+
+			age = 23;
+
+			name = "Brianna";
+
+			isAlive = true;
+
+			Console.WriteLine($"({name} {age} is she alive: {isAlive}"); 
+		}
+	}
+}
